4be5584234d1a374a1433863924119b33988a255
Bra Estudio Web <braestudioweb@gmail.com>
2025-11-06 02:47:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4093137218-14e2b",
+  "appId": "1:836856057193:web:a3428117033c45b75641f8",
+  "apiKey": "AIzaSyD_K_zyZseeBOTaFbpNl-Zlg74S_tQMcjY",
+  "authDomain": "studio-4093137218-14e2b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "836856057193"
+};
